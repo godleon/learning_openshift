d31273c0119bc68e634ad77fd11ffeb5c4e336d9
godleon <godleon@gmail.com>
2017-08-08 10:21:02 +0800

    change python script

--- examples/ansible-lint/app.py
+++ examples/ansible-lint/app.py
@@ -1,1 +1,21 @@
+#!/usr/bin/python3
+
+import time
+
+print("Hello Ansible Lint (this is in app.py)")
+
+time.sleep(10)
+
+print("Hello Ansible Lint (this is in app.py)")
+
+time.sleep(10)
+
+print("Hello Ansible Lint (this is in app.py)")
+
+time.sleep(10)
+
+print("Hello Ansible Lint (this is in app.py)")
+
+time.sleep(10)
+
 print("Hello Ansible Lint (this is in app.py)")
