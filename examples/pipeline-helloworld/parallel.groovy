8f21ec0494b9c3cafee8cad473073b2c7e827495
godleon <godleon@gmail.com>
2017-08-13 05:04:27 +0800

    add jenkinsfilePath

---
+++ examples/pipeline-helloworld/parallel.groovy
@@ -0,0 +1,33 @@
+node {
+    stage('build 1') {
+        openshiftBuild(buildConfig: 'bc1', showBuildLogs: 'true')
+    }
+
+    // stage('build 2') {
+    // parallel {
+    //     "parallel 2.1": {
+    //     node {
+    //         openshiftBuild(buildConfig: 'bc2-1', showBuildLogs: 'true')
+    //     }
+    //     }, 
+    //     "parallel 2.2": {
+    //     node {
+    //         openshiftBuild(buildConfig: 'bc2-2', showBuildLogs: 'true')
+    //     }
+    //     }
+    // }
+    // }
+
+    stage('build 3') {
+        openshiftBuild(buildConfig: 'bc3', showBuildLogs: 'true')
+    }
+
+    // stage('job 1') {
+    // sh "oc delete pod/pi"
+
+    // sh "oc run pi --image=ubuntu --replicas=1 --restart=OnFailure --command -- echo 'Job in Ubuntu Image'; sleep 10; echo 'Job is done'"
+
+    // sh "oc logs pod/pi"
+    // }
+    
+}
---
+++ jenkins-integration/README.md
@@ -0,0 +1,7 @@
+
+
+
+References
+==========
+
+- [Orchestrating Jobs On Openshift And Kubernetes](http://cpitman.github.io/openshift/jenkins/2017/04/02/orchestrating-jobs-on-openshift-and-kubernetes.html#.WY9hR3WGOAk)
