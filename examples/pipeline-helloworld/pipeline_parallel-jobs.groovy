6dc114a1b2fc1437537d065c56172f2213d2d220
godleon <godleon@gmail.com>
2017-08-13 05:40:12 +0800

    add parallel jobs

--- examples/pipeline-helloworld/pipeline_parallel-jobs.groovy
+++ examples/pipeline-helloworld/pipeline_parallel-jobs.groovy
@@ -1,32 +1,28 @@
-// node {
-//     stage('build 1') {
-//         openshiftBuild(buildConfig: 'bc1', showBuildLogs: 'true')
-//     }
+stage ("task 1") {
+  node {
+    writeFile file: 'job.yaml', text: formatJob(env.BUILD_ID, "1")
+    sh "oc create -f job.yaml"
+  }
+}
 
-//     // stage('build 2') {
-//     // parallel {
-//     //     "parallel 2.1": {
-//     //     node {
-//     //         openshiftBuild(buildConfig: 'bc2-1', showBuildLogs: 'true')
-//     //     }
-//     //     }, 
-//     //     "parallel 2.2": {
-//     //     node {
-//     //         openshiftBuild(buildConfig: 'bc2-2', showBuildLogs: 'true')
-//     //     }
-//     //     }
-//     // }
-//     // }
 
-//     stage('build 3') {
-//         openshiftBuild(buildConfig: 'bc3', showBuildLogs: 'true')
-//     }
 
-//     // stage('job 1') {
-//     // sh "oc delete pod/pi"
-
-//     // sh "oc run pi --image=ubuntu --replicas=1 --restart=OnFailure --command -- echo 'Job in Ubuntu Image'; sleep 10; echo 'Job is done'"
-
-//     // sh "oc logs pod/pi"
-//     // }    
-// }
+def formatJob(buildId, id) {
+  """
+apiVersion: batch/v1
+kind: Job
+metadata:
+  name: curl-${buildId}-${id}
+spec:
+  template:
+    metadata:
+      name: curl-${buildId}-${id}
+    spec:
+      activeDeadlineSeconds: 60
+      containers:
+      - name: ubuntu-job-${buildId}-${id}
+        image: ubuntu
+        command: ["echo", "job-${buildId}-${id} in Ubuntu image"]
+      restartPolicy: Never
+  """
+}
