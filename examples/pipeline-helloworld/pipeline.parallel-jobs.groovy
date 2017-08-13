0f73aca221a5d91370624c0257d62a1114f8dd55
godleon <godleon@gmail.com>
2017-08-13 18:07:43 +0800

    test

--- examples/pipeline-helloworld/pipeline.parallel-jobs.groovy
+++ examples/pipeline-helloworld/pipeline.parallel-jobs.groovy
@@ -1,22 +1,46 @@
-node {
+// node {
 
-    stage('build 1') {
-        openshiftBuild(buildConfig: 'bc1', showBuildLogs: 'true')
-    }
+//     stage('build 1') {
+//         openshiftBuild(buildConfig: 'bc1', showBuildLogs: 'true')
+//     }
 
-    stage('build 2 parallel') {
-      parallel branchA: {
-        openshiftBuild(buildConfig: 'bc2-1', showBuildLogs: 'true')
-      }, branchB: {
-        openshiftBuild(buildConfig: 'bc2-2', showBuildLogs: 'true')
-      }
-    }
+//     stage('build 2 parallel') {
+//       parallel branchA: {
+//         openshiftBuild(buildConfig: 'bc2-1', showBuildLogs: 'true')
+//       }, branchB: {
+//         openshiftBuild(buildConfig: 'bc2-2', showBuildLogs: 'true')
+//       }
+//     }
 
-    stage('build 3') {
-        openshiftBuild(buildConfig: 'bc3', showBuildLogs: 'true')
+//     stage('build 3') {
+//         openshiftBuild(buildConfig: 'bc3', showBuildLogs: 'true')
+//     }
+// }
+
+
+stage('build 1') {
+    node {
+      openshiftBuild(buildConfig: 'bc1', showBuildLogs: 'true')
     }
 }
 
+stage('build 2 parallel') {
+  parallel branchA: {
+    node {
+      openshiftBuild(buildConfig: 'bc2-1', showBuildLogs: 'true')
+    }
+  }, branchB: {
+    node {
+      openshiftBuild(buildConfig: 'bc2-2', showBuildLogs: 'true')
+    }
+  }
+}
+
+stage('build 3') {
+    node {
+      openshiftBuild(buildConfig: 'bc3', showBuildLogs: 'true')
+    }
+}
 
 
 
