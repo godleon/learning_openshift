326d541da449823cb0834ae15accee6bd3aa5318
godleon <godleon@gmail.com>
2017-11-07 16:59:40 +0800

    test serviceaccount in pipeline

--- jenkins_pipeline/hello-world.groovy
+++ jenkins_pipeline/hello-world.groovy
@@ -2,6 +2,16 @@ openshift.withCluster() {
 
     echo "Hello from the project running Jenkins: ${openshift.project()}"
 
+    def saSelector = openshift.selector('serviceaccount')
+    saSelector.describe()
+
+    // Selectors also allow you to easily iterate through all objects they currently select.
+    saSelector.withEach { // The closure body will be executed once for each selected object.
+        // The 'it' variable will be bound to a Selector which selects a single
+        // object which is the focus of the iteration.
+        echo "Service account: ${it.name()} is defined in ${openshift.project()}"
+    }
+
     openshift.withProject() {
         echo "Hello from project ${openshift.project()} in cluster ${openshift.cluster()}"
     }
