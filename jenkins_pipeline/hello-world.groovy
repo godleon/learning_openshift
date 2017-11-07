76b68db24f6834828ba25570cbbb2ea9e63d27f6
godleon <godleon@gmail.com>
2017-11-07 16:47:06 +0800

    jenkins pipeline test

--- jenkins_pipeline/hello-world.groovy
+++ jenkins_pipeline/hello-world.groovy
@@ -1,7 +1,9 @@
 openshift.withCluster() {
 
+    echo "Hello from the project running Jenkins: ${openshift.project()}"
+
     openshift.withProject() {
         echo "Hello from project ${openshift.project()} in cluster ${openshift.cluster()}"
     }
-    
+
 }
